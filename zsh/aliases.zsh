c5d4da1079b50ad6e60fdcd2e4fc81ef1221351d
Eumir Gaspar <eumirgaspar@gmail.com>
2019-11-06 09:20:38 +1100

    Initial commit

---
+++ zsh/aliases.zsh
@@ -0,0 +1,10 @@
+# Most of these are copied from https://github.com/skwp/dotfiles/blob/4f96ebb0aced3730e2ae276cc7053f0d8e611a60/zsh/aliases.zsh
+#
+
+home=~/Documents/Projects/neodotfiles
+
+alias ae='nvim $home/zsh/aliases.zsh' #alias edit
+alias ar='source $home/zsh/aliases.zsh'  #alias reload
+
+# GIT
+alias gs='git status'
