7c6a9c764c4b0ea12adb3c6f520b9f8a266979ea
Eumir Gaspar <eumirgaspar@gmail.com>
2019-11-06 09:25:48 +1100

    Added more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,3 +8,14 @@ alias ar='source $home/zsh/aliases.zsh'  #alias reload
 
 # GIT
 alias gs='git status'
+alias gst='git stash'
+alias gsp='git status --pretty'
+alias gcm='git commit -m'
+alias gap='git add -p'
+alias gbi='git rebase --interactive'
+alias gl='git l'
+alias gdc='git diff --cached -w'
+alias gdmb='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+
+# Ruby
+alias be='bundle exec'
