c0d10c31ff0d8d74bb8ea2be5a01652ae7c985fb
Eumir Gaspar <eumirgaspar@gmail.com>
2022-08-10 17:05:51 +0800

    Added more shortcuts and use HOME vs home

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,12 +4,13 @@
 home=$HOME/Projects/neodotfiles
 
 # ZSH
-alias ac='cat $home/zsh/aliases.zsh'        #alias check
-alias ae='nvim $home/zsh/aliases.zsh' 	    #alias edit
-alias ar='source $home/zsh/aliases.zsh'     #alias reload
-alias az='nvim $home/zsh/.zshrc' 	          #edit zshrc
-alias azr='source ~/.zshrc'		              #reload zshrc
-alias ast='nvim ~/.config/starship.toml'    #edit starship
+alias ak='nvim $HOME/.config/kitty/kitty.conf'  #edit kitty.conf
+alias ac='cat $home/zsh/aliases.zsh'            #alias check
+alias ae='nvim $home/zsh/aliases.zsh'           #alias edit
+alias ar='source $home/zsh/aliases.zsh'         #alias reload
+alias az='nvim $HOME/.zshrc'    	              #edit zshrc
+alias azr='source $HOME/.zshrc'		              #reload zshrc
+alias ast='nvim $HOME/.config/starship.toml'    #edit starship
 
 # VIM
 alias vi='nvim'
